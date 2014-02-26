f915b04b58b13a36838d146ebd4ef987d5280773
Daniel Torres <danielfeelfine@gmail.com>
2014-02-26 05:03:48 -0300

    Update app/index.js
    Add platform options with media files

--- app/index.js
+++ app/index.js
@@ -19,21 +19,36 @@ var MbpGenerator = yeoman.generators.Base.extend({
   askFor: function () {
     var done = this.async();
 
-    // have Yeoman greet the user
-    console.log(this.yeoman);
-
-    // replace it with a short and sweet description of your generator
-    console.log(chalk.magenta('You\'re using the Mobile Blueprints generator. Out of the box I include Zepto.js, iScroll, HammerJS and FastClick libraries for help to build your Mobile app.'));
+    // welcome message
+    if (!this.options['skip-welcome-message']) {
+      console.log(this.yeoman);
+      console.log(chalk.magenta('You\'re using the Mobile Blueprints generator. Out of the box I include Zepto.js, iScroll, HammerJS and FastClick libraries for help to build your Mobile app.'));
+    }
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to create that scaffold now?',
-      default: true
+      type: 'checkbox',
+      name: 'platforms',
+      message: 'What platforms you build?',
+      choices: [{
+        name: 'iOS',
+        value: 'platformiOS',
+        checked: true
+      }, {
+        name: 'Android',
+        value: 'platformAndroid',
+        checked: false
+      }]
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    this.prompt(prompts, function (answers) {
+      var platforms = answers.platforms;
+
+      function hasPlatform(feat) { return platforms.indexOf(feat) !== -1; }
+
+      // manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.platformiOS = hasPlatform('platformiOS');
+      this.platformAndroid = hasPlatform('platformAndroid');
 
       done();
     }.bind(this));
@@ -46,6 +61,20 @@ var MbpGenerator = yeoman.generators.Base.extend({
     this.mkdir('merges');
 
     this.copy('_package.json', 'package.json');
+
+    if (this.platformiOS) {
+      this.write(
+        'www/stylesheets/css/media/ios/iphone-5.css',
+        'console.log "\'Allo from iOS!"'
+      );
+    }
+
+    if (this.platformAndroid) {
+      this.write(
+        'www/stylesheets/css/media/android/galaxy-s3.css',
+        'console.log "\'Allo from Android!"'
+      );
+    }
   },
 
   bower: function () {
