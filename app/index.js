4beaa973582e8f3970cffbba4b337a17fc045767
Daniel Torres <danielfeelfine@gmail.com>
2014-05-01 15:38:19 -0300

    Update app/index.js
    add www/config.xml in appfiles

--- app/index.js
+++ app/index.js
@@ -77,6 +77,7 @@ var FastgapGenerator = yeoman.generators.Base.extend({
   },
 
   appfiles: function () {
+    this.copy('www/config.xml', 'www/config.xml');
     this.copy('www/index.html', 'www/index.html');
   },
 
