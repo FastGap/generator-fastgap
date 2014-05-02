619ceb86056ea978330bec5e3bbd02735e9edc10
Daniel Torres <danielfeelfine@gmail.com>
2014-05-02 17:02:36 -0300

    Update templates/Gruntfile.js
    add appConfig and banner template

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,4 +2,31 @@
 module.exports = function (grunt) {
   'use strict';
 
+  var appConfig = {
+
+    // Default Paths
+    path: {
+      src: 'src',
+      www: 'www',
+      assets: 'www/assets',
+      fonts: 'src/data/fonts',
+      styles: 'src/styles',
+      controllers: 'src/controllers',
+      views: 'src/views'
+    },
+
+    // Metadata
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+            ' * <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * <%= pkg.description %>\n' +
+            ' * Licensed under <%= pkg.license %>\n' +
+            ' */\n',
+
+
+  };
+
+  // Init grunt configurations
+  grunt.initConfig(appConfig);
+
 };
