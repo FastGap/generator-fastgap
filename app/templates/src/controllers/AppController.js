78ca4d5d689c1c771c96536fd64e740d61c6c151
Daniel Torres <danielfeelfine@gmail.com>
2014-05-03 18:41:48 -0300

    Add templates/src/controllers/AppController.js
    with fastgap@0.0.49 demo

---
+++ app/templates/src/controllers/AppController.js
@@ -0,0 +1,21 @@
+/*!
+ * FastGap v0.0.49 (http://fastgap.mobi/)
+ * Author: Gustavo Costa
+ * Maintainers: https://github.com/orgs/FastGap/members
+ * Copyright (c) 2014
+ * Licensed under MIT
+ */
+
+var AppController = function () {};
+
+AppController.prototype = {
+	initialize: function () {
+
+		//YOUR "GLOBAL CODE" HERE.
+
+
+	},
+	destroy: function () {
+		PageLoad.ajxHandle = null;
+	}
+};
